6da8ea91f1d7c88115c6874a999860696991dc24
aruhi-music <81058621+aruhi-music@users.noreply.github.com>
2022-03-21 13:13:01 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,143 @@
+window.onload = function () {
+    //スクロール量を取得する関数
+    function getScrolled() {
+        return ( window.pageYOffset !== undefined ) ? window.pageYOffset: document.documentElement.scrollTop;
+    }
+    // 歌詞表示
+    // 赤い靴
+    document.getElementById("red_shoes").onclick = function() {
+        document.getElementById("display_title").style ="display:none";
+        document.getElementById("display_write").style ="display:none";
+        document.getElementById("display_make").style ="display:none";
+        document.getElementById("display_lyric").style ="display:none";
+        document.getElementById("display_title").innerHTML = "赤い靴";
+        document.getElementById("display_write").innerHTML = "作詞:室山颯";
+        document.getElementById("display_make").innerHTML = "作曲:室山颯";
+        document.getElementById("display_lyric").innerHTML = "家は貧しく、裸足で過ごして<br>足を怪我した、そんなある時<br>足を怪我した、そんなある時<br>一人の靴屋が恵んでくれた<br>赤い靴をボロボロになるまで履いた<br><br>母が亡くなり引き取られた私は<br>町で一番の美人に育った<br>王女が身につけた、一つの憧れ<br>ある時に靴屋で同じモノ見つけて<br><br>その美しさに、心奪われ<br>欲望に打ち負けた私は抑えきれなかった<br><br>どこへ行く時も、どんな時でも、<br>いつも履いて出かけて<br>何をする時も、考えること<br>頭の中赤い靴のことだけ<br><br>誰の注意にも聞く耳はもたず<br>我儘、思いのままに過ごした<br>そんな私には呪いがかけられ<br>靴が脱げない足が止まらない<br><br>踊り狂って、疲れ果てたわ<br>この赤い呪いと一緒に私の足切り落として<br><br>いくら償っても、祈り捧げても<br>赦されることはなくて<br>受けた恩忘れ、自分勝手で<br>そんな奴に天国も地獄もない<br>";
+        $('p').fadeIn(900); 
+    };
+    // Q.E.D
+    document.getElementById("qed").onclick = function() {
+        document.getElementById("display_title").style ="display:none";
+        document.getElementById("display_write").style ="display:none";
+        document.getElementById("display_make").style ="display:none";
+        document.getElementById("display_lyric").style ="display:none";
+        document.getElementById("display_title").innerHTML = "Q.E.D";
+        document.getElementById("display_write").innerHTML = "作詞:山﨑博士";
+        document.getElementById("display_make").innerHTML = "作曲:山﨑博士";
+        document.getElementById("display_lyric").innerHTML = "静寂を奪い取ってしまう君は<br>僕にとっての天使か悪魔か<br>どちらに転んでも<br>害はない故にわからない<br>あ〜どうすればいい<br><br>声かけてただ遠のいてしまわぬように<br>スタイルを決めて<br>それでもさ上手くいかなくて<br>ただ季節が流れてく<br><br>あの時あの場所で<br>出会えたことが今の僕の証明<br>小さな波紋が広がってゆくように<br>また君を好きになるから<br>明日には昨日のことを<br>笑いあって話せたら良いな<br>この気持ちを恋と呼ぶなら<br>僕はその問いの解答者<br>Q.E.D. Q.E.D. 見つけなくちゃ<br><br>ひとり＋ひとりの答えはふたり<br>こんな公式じゃ<br>君＋僕に当てはまらない<br>だから足して引いて掛けて割ってみたけど<br>あ〜どうすればいい<br><br>声もかけず<br>遠巻きに見ているだけでは<br>何も始まらない<br>それではさ上手くいくことも<br>確率は0%<br><br>あの時あの場所で<br>出会えたことが今の僕の証明<br>小さな奇跡が重なってく度に<br>また君を好きになるから<br><br>明日には昨日のことを<br>笑いあって話せたら良いな<br>この気持ちを恋と呼ぶなら<br>僕はその問いの解答者<br>Q.E.D. Q.E.D. 探さなくちゃ<br><br>Q.E.D. 〜Q.E.D. Q.E.D.<br><br>あの時あの場所で<br>出会えたことが今の僕の証明<br>小さな波紋が広がってゆくように<br>また君を好きになるから<br>だから僕は君の眼の前で<br>その答えを最後に解こうか<br>この気持ちを恋と呼ぶなら<br>僕はその問いの解答者<br>Q.E.D. Q.E.D.  君が好き<br>";
+        $('p').fadeIn(900); 
+    };
+    // 正義
+    document.getElementById("justice").onclick = function() {
+        document.getElementById("display_title").style ="display:none";
+        document.getElementById("display_write").style ="display:none";
+        document.getElementById("display_make").style ="display:none";
+        document.getElementById("display_lyric").style ="display:none";
+        document.getElementById("display_title").innerHTML = "正義";
+        document.getElementById("display_write").innerHTML = "作詞:室山颯";
+        document.getElementById("display_make").innerHTML = "作曲:室山颯";
+        document.getElementById("display_lyric").innerHTML = "いつになっても<br>間違った奴らの主張が罷り通る<br>嫌気が差すほど<br>腐った世の中の常識に押し潰されていく<br><br>何の為の正義で誰を守り<br>犠牲には目を瞑って何が正義だ<br><br>表の顔と裏の顔を<br>上手いこと人は使い分ける<br>金と権力と地位と名誉を<br>大義名分に奪い合い<br><br>誰かを守る振りをして自分を守り<br>多勢の見方の上で何をかざす<br><br>何の為の正義で誰を守り<br>犠牲には目を瞑って何が正義だ<br>";
+        $('p').fadeIn(900); 
+    };
+    // 生き様と死に様
+    document.getElementById("dead_alive").onclick = function() {
+        document.getElementById("display_title").style ="display:none";
+        document.getElementById("display_write").style ="display:none";
+        document.getElementById("display_make").style ="display:none";
+        document.getElementById("display_lyric").style ="display:none";
+        document.getElementById("display_title").innerHTML = "生き様と死に様";
+        document.getElementById("display_write").innerHTML = "作詞:室山颯";
+        document.getElementById("display_make").innerHTML = "作曲:室山颯";
+        document.getElementById("display_lyric").innerHTML = "上手くいかないこともある<br>その度にイライラがつのる<br>他人を変えることなんて<br>誰にもできやしないよ<br><br>嫌いな上司の機嫌が悪かった<br>態度の悪い客にムカついた<br>そんな毎日に嫌気が差した <br><br>気楽にいこうぜ<br>誰が何を言おうとしようと関係ないのさ<br>所詮他所は他所、好きなようにやるんだ<br>僕は僕らしく、君は君らしく、<br>ありのままで生きていけばいいさ<br><br>なるようになるとただ生きてきた<br>将来なんてどうでもよかった<br>普通の人になれればよかった<br><br>誰のせいでもない<br>自分で選んだ道は 自分のせいだ<br>ただ辿り着く先に待ってるものは同じだ<br>いつか必ず、誰しも必ず、<br>死にゆくなら今は楽しもうか<br>";
+        $('p').fadeIn(900); 
+    };
+    // 欲しかったもの
+    document.getElementById("want").onclick = function() {
+        document.getElementById("display_title").style ="display:none";
+        document.getElementById("display_write").style ="display:none";
+        document.getElementById("display_make").style ="display:none";
+        document.getElementById("display_lyric").style ="display:none";
+        document.getElementById("display_title").innerHTML = "ホシカッタモノ";
+        document.getElementById("display_write").innerHTML = "作詞:山﨑博士";
+        document.getElementById("display_make").innerHTML = "作曲:山﨑博士";
+        document.getElementById("display_lyric").innerHTML = "ホシカッタモノって何だっけ？<br>気付いているうちに　消えていた<br>どうして　君はそばに　<br>このままいてくれないのかな<br><br>あんなに　大事にしてたものは　<br>どこへいったのかわからない<br>どうして　触れた数だけ　あなたの痛みが　<br>増えていった<br><br>消えない思い　苦しいこと　始めたものは　<br>星の数あるよ<br>初めてだ　それは初めてだ　どうやら<br>この先も　この先も　この先も　残るよ<br><br>いつの日にか　この意味も変わり<br>同じように　締め付ける日々は<br>いつの間にか　隣に追いやって ほら<br>僕らは　描いた傷を眺めていた<br><br>ホシカッタモノって　何だっけ？<br>与えられても　まだ　遠い目をしている<br>上書きしたくて　塗り潰すごとに<br>何が何だか　わからないんだよ　　<br><br>足りない思い　生まれる孤独　初めから全部　<br>指折り数えたら<br>そこにいた　いつも　そこにいた　ような<br>気がしてる　気がしてる　気がしてる　だけなんだ<br><br>いつの日にか　この意味も変わり<br>同じように　締め付ける日々は<br>いつの間にか　隣に追いやって ほら<br>僕らは　描いた傷を眺めていた<br><br>このままじゃ　いけないからって　言われたって<br>答えてよ　教えてよ　消してよ<br><br>大事なものは　いつもそばにあって<br>きっと　二択を　迫られる日々に<br>ずっと　悩むこと　これからも　続くから　<br>僕らは　互いに　傷を　蝕んでいく<br><br>いつの日にか　この意味も変わり<br>同じように　締め付ける日々は<br>いつの間にか　隣に追いやって ほら<br>僕らは　描いた傷を眺めていた<br>";
+        $('p').fadeIn(900); 
+    };
+    // 夏がくるたび
+    document.getElementById("summer").onclick = function() {
+        document.getElementById("display_title").style ="display:none";
+        document.getElementById("display_write").style ="display:none";
+        document.getElementById("display_make").style ="display:none";
+        document.getElementById("display_lyric").style ="display:none";
+        document.getElementById("display_title").innerHTML = "夏が来るたび";
+        document.getElementById("display_write").innerHTML = "作詞:室山颯";
+        document.getElementById("display_make").innerHTML = "作曲:室山颯";
+        document.getElementById("display_lyric").innerHTML = "雨の日が続いてる 少し重たいな<br>予報によればやっと梅雨が明けるみたいだ<br>何日か眠ればもうすぐ季節が巡る<br><br>空は青く高くどこまでも遠くて<br>光を仰ぎ目を閉じる<br><br>夏が来るたび思い出がそこにはあって<br>懐かしい日々に僕を連れ出してくれる<br><br>焼けるような暑い日が続いていた<br>疲れは溜まり無駄な日を過ごしてしまうけど<br>日が短くなる壁のカレンダーをめくる<br><br>海は碧く冷たくどこまでも深くて<br>闇に飛び込み泳いでいく<br><br>夏の終わりに薄れゆく記憶<br>今年の最後の花火が夜空に上がる<br><br>夏が来るたび思い出がそこにはあって<br>懐かしい日々に僕を連れ出してくれる<br><br>夏が来るたび<br>新しい日々に僕を連れて出してくれる<br>";
+        $('p').fadeIn(900); 
+    };
+    // Witch
+    document.getElementById("witch").onclick = function() {
+        document.getElementById("display_title").style ="display:none";
+        document.getElementById("display_write").style ="display:none";
+        document.getElementById("display_make").style ="display:none";
+        document.getElementById("display_lyric").style ="display:none";
+        document.getElementById("display_title").innerHTML = "Witch";
+        document.getElementById("display_write").innerHTML = "作詞:室山颯";
+        document.getElementById("display_make").innerHTML = "作曲:室山颯";
+        document.getElementById("display_lyric").innerHTML = "十五になると広がる外の世界で<br>美しくも儚い出会いの物語<br><br>お前が来ること私は分かっていたわ<br>願い事なら全部叶えてあげる<br><br>ただしその代償に大事なもの頂くよ<br><br>【例えどんな痛みもどんな苦しみも<br>あたし受け入れる覚悟はあるわ】<br>それならこの薬を口にしてみなよ<br>その代わりもう二度とは戻れないよ<br><br>【お願い気付いてあたしはここよ<br>このままでは消えてしまうわ】<br>助かる為のただ唯一の方法<br>そのナイフを胸にひと突きするの<br><br>【愛する貴方との日々終わってしまうの？】<br>どちらにせよこれでもうお終いさ<br>【別れを告げてそのナイフも捨てて】<br>最後は海に身を投げ泡となるの<br>";
+        $('p').fadeIn(900); 
+    };
+    // 友達以上恋人未満
+    document.getElementById("friend_lover").onclick = function() {
+        document.getElementById("display_title").style ="display:none";
+        document.getElementById("display_write").style ="display:none";
+        document.getElementById("display_make").style ="display:none";
+        document.getElementById("display_lyric").style ="display:none";
+        document.getElementById("display_title").innerHTML = "友達以上恋人未満";
+        document.getElementById("display_write").innerHTML = "作詞:山﨑博士";
+        document.getElementById("display_make").innerHTML = "作曲:山﨑博士";
+        document.getElementById("display_lyric").innerHTML = "夜の街を歩く彼氏彼女は今日も<br>偽りとプライドの似通った加工で<br>騙し騙され合いなんて日常茶飯事<br><br>自らの欲望を満たすために明日も<br>取っ替え引っ替え相手をかえて <br>満足げにビールを飲んで眠る日々<br><br>それだけでは癒されないでも<br>温もりを感じていたいから <br>寂しくないようにそっと<br>君の名を呼ぶんだ<br><br>ねぇ ふたりの間に愛以上の感情はない<br>このまま燃えるような夜を過ごそうよ<br>ほらただ重なって繋ぎ合わせて<br>見えない快楽に溺れてしまっても <br>ふたりはそれでも求めるよ 「愛」<br><br>新宿の街を染め上げる灯り<br>今日もどこか寂しげなふたり<br>照らし続けるネオンの光<br>寄り添って惑わし引き裂く<br>価値観のずれ違和感の群れ <br>飛び込んだ上でもなお<br>何も残らない胸の奥　穴が空く<br><br>はじめましてこんにちは<br>出会いを求めて会いにきました<br>寂しさを紛らわすための関係で良いのか?<br><br>ふたりの間に、、、<br>このまま燃え尽きて<br>全てを忘れてしまいたいのに<br><br>ふたりの間に愛以上の感情はない<br>このまま燃えるよう夜を過ごそうよ<br>ほらただ重なって繋ぎ合わせて<br>見えない快楽に溺れてしまっても<br>ふたりはこれからも求めるよ「愛」<br>";
+        $('p').fadeIn(900); 
+    };
+    // Joker
+    document.getElementById("joker").onclick = function() {
+        document.getElementById("display_title").style ="display:none";
+        document.getElementById("display_write").style ="display:none";
+        document.getElementById("display_make").style ="display:none";
+        document.getElementById("display_lyric").style ="display:none";
+        document.getElementById("display_title").innerHTML = "Joker";
+        document.getElementById("display_write").innerHTML = "作詞:室山颯";
+        document.getElementById("display_make").innerHTML = "作曲:室山颯";
+        document.getElementById("display_lyric").innerHTML = "時にモノクロで 時に最悪な<br>人々が恐れ慄いていくジョーカー<br><br>時にカラフルで 時に最強な<br>誰しもが一度は手にしてみたいもんさ<br><br>決して触れてはいけないこと<br>でも私には関係ない<br>何でもありよ<br>恐れなんて知らない<br>唯一無二の存在<br>私があなたの切り札よ<br><br>誰も触れてはいけないこと<br>でも私には関係ない<br>何でもありよ<br>恐れなんて知らない<br>唯一無二の存在<br>私があなたの切り札よ<br>";
+        $('p').fadeIn(900); 
+    };
+    //トップに戻るボタンの要素を取得
+    var topButton = document.getElementById( 'js-scroll-fadein' );
+                
+    //ボタンの表示・非表示
+    window.onscroll = function() {
+        ( getScrolled() > 100 ) ? topButton.classList.add( 'is-fadein' ): topButton.classList.remove( 'is-fadein' );
+    };
+                
+    //トップに移動する関数
+    function scrollToTop() {
+        var scrolled = getScrolled();
+        window.scrollTo( 0, Math.floor( scrolled / 2 ) );
+        if ( scrolled > 0 ) {
+        window.setTimeout( scrollToTop, 30 );
+        }
+    };
+                
+    //イベント登録
+    topButton.onclick = function() {
+        scrollToTop();
+    };
+
+    // スクロール表示アニメーション
+    ScrollReveal().reveal('.scroll-fade',{
+        duration: 1600, // アニメーションの完了にかかる時間
+        origin: 'bottom', // どこから現れるか
+        distance: '50px'
+    });
+};
